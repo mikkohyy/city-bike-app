1ebf15b0d43a6d200c6328a671e5d5793057d2a1
mikko <mikko.e.hyyrylainen@gmail.dom>
2023-01-05 16:20:28 +0200

    change depatureStationId's type from string to StationId

--- server/types.ts
+++ server/types.ts
@@ -1,10 +1,32 @@
 export type StationId = string
 
+export type XCoordinate = number
+
+export type YCoordinate = number
+
 export interface NewJourney {
   departureTime: string
   returnTime: string
-  departureStationId: string
+  departureStationId: StationId
   returnStationId: StationId
   coveredDistanceInMeters: number
   durationInSeconds: number
 }
+
+export interface NewStation {
+
+}
+
+    stationId: station.ID,
+    nameInFinnish: station.Name,
+    nameInSwedish: station.Namn,
+    nameInEnglish: station.Name,
+    addressInFinnish: station.Osoite,
+    addressInSwedish: station.Adress,
+    addressInFinnish: station.Osoite,
+    cityInFinnish: station.Kaupunki,
+    cityInSwedish: station.Stad,
+    operator: station.Operaattor,
+    capacity: station.Kapasiteet,
+    xCoordinate: station.x,
+    yCoordinate: station.y,
