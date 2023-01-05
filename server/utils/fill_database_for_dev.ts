14035a25ae90e7d5d4097fc560edd20ff348dbde
mikko <mikko.e.hyyrylainen@gmail.dom>
2023-01-05 10:07:12 +0200

    fix typo depature => departure

--- server/utils/fill_database_for_dev.ts
+++ server/utils/fill_database_for_dev.ts
@@ -1,12 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { connectToDatabase } from './db'
-import { Station } from '../models'
+import { Station, Journey } from '../models'
 import fs from 'fs'
 import { parse } from 'csv-parse/sync'
 import path from 'path'
+import { toNewJourney } from './input_proofing'
+import { NewJourney } from '../types'
 
-const csvFilePath = path.resolve(__dirname, '../dev_data/stations.csv')
-const headers = [
+const how = process.argv[2]
+
+const stationCsvFilePath = path.resolve(__dirname, '../dev_data/stations.csv')
+const mayJourneysFilePath = path.resolve(__dirname, '../dev_data/2021-05.csv')
+// const juneJourneysFilePath = path.resolve(__dirname, '../dev_data/2021-06.csv')
+// const julyJourneysFilePath = path.resolve(__dirname, '../dev_data/2021-05.csv')
+
+const stationHeaders = [
   'FID',
   'ID',
   'Nimi',
@@ -22,12 +30,29 @@ const headers = [
   'y',
 ]
 
-const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' })
+const journeyHeaders = [
+  'Departure',
+  'Return',
+  'Departure station id',
+  'Departure station name',
+  'Return station id',
+  'Return station name',
+  'Covered distance (m)',
+  'Duration (sec.)',
+]
+
+const stationsFileContent = fs.readFileSync(stationCsvFilePath, {
+  encoding: 'utf-8',
+})
+
+const mayJourneysFileContent = fs.readFileSync(mayJourneysFilePath, {
+  encoding: 'utf-8',
+})
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const stationData = parse(fileContent, {
+const stationData = parse(stationsFileContent, {
   delimiter: ',',
-  columns: headers,
+  columns: stationHeaders,
   from_line: 2,
 })
 
@@ -49,12 +74,52 @@ const formattedStationData = stationData.map((station: any) => {
   }
 })
 
-const addToDatabase = async () => {
+const getFormattedJourneyData = (fileContent: string): NewJourney[] => {
+  const journeyDataNow: NewJourney[] = []
+  const journeysData: [] = parse(fileContent, {
+    delimiter: ',',
+    columns: journeyHeaders,
+    from_line: 2,
+  })
+
+  for (const journey of journeysData) {
+    try {
+      const formattedJourney = toNewJourney(journey)
+      journeyDataNow.push(formattedJourney)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return journeyDataNow
+}
+
+const formattedMayJourneysData = getFormattedJourneyData(mayJourneysFileContent)
+
+const addStationsToDatabase = async () => {
   await connectToDatabase()
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   await Station.bulkCreate(formattedStationData, { validate: true })
 }
 
-void addToDatabase()
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const addJourneysToDatabase = async (data: any) => {
+  await connectToDatabase()
+  const chunkSize = 100
+  for (let i = 0; i < data.length; i += chunkSize) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const chunk = data.slice(i, i + chunkSize)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    await Journey.bulkCreate(chunk, { validate: true })
+  }
+}
+
+if (how !== 'journeys-o') {
+  void addStationsToDatabase()
+}
+
+if (how !== 'stations') {
+  void addJourneysToDatabase(formattedMayJourneysData)
+}
 
 export {}
