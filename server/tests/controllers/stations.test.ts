03f2cdf2cfa692e3d05ac8ccebf4b07e2c16981b
mikko <mikko.e.hyyrylainen@gmail.dom>
2023-01-08 13:33:37 +0200

    add user can give pagination parameters

--- server/tests/controllers/stations.test.ts
+++ server/tests/controllers/stations.test.ts
@@ -19,6 +19,15 @@ import { sequelize } from '../../utils/db'
 import { Response } from 'superagent'
 const queryInterface = sequelize.getQueryInterface()
 
+const EXPECTED_RETURN_OBJECT_KEYS = [
+  'totalNOfRows',
+  'page',
+  'pageSize',
+  'nextPage',
+  'totalPages',
+  'data',
+]
+
 beforeAll(async () => {
   await resetDatabase(queryInterface)
   await prepareDatabase()
@@ -58,16 +67,95 @@ describe('Stations route', () => {
       test('responds with status code 200', () => {
         expect(receivedData.statusCode).toBe(200)
       })
-      describe('returned array', () => {
-        test('has the expected length', () => {
-          expect(receivedData.body.data).toHaveLength(
-            stationsInCamelCaseWithSetDatabaseId.length
-          )
+      describe('response object', () => {
+        test('has the right number of properties', () => {
+          const keysInObject = Object.keys(receivedData.body)
+          expect(keysInObject).toHaveLength(EXPECTED_RETURN_OBJECT_KEYS.length)
         })
-        test('has expected stations', () => {
-          for (const station of stationsInCamelCaseWithSetDatabaseId) {
-            expect(receivedData.body.data).toContainEqual(station)
+        test('has the expected properties and values (excluding station data)', () => {
+          const responseObject = receivedData.body
+
+          const expectedObject: { [index: string]: unknown } = {
+            totalNOfRows: 10,
+            page: 0,
+            pageSize: 50,
+            nextPage: null,
+            totalPages: 1,
           }
+
+          for (const key of Object.keys(responseObject)) {
+            if (key !== 'data') {
+              expect(responseObject[key]).toBe(expectedObject[key])
+            }
+          }
+        })
+        describe('stations in data property', () => {
+          test('has the expected length', () => {
+            expect(receivedData.body.data).toHaveLength(
+              stationsInCamelCaseWithSetDatabaseId.length
+            )
+          })
+          test('has expected stations', () => {
+            for (const station of stationsInCamelCaseWithSetDatabaseId) {
+              expect(receivedData.body.data).toContainEqual(station)
+            }
+          })
+        })
+      })
+    })
+  })
+  describe('GET /api/stations?page=0&limit=5', () => {
+    let receivedData: Response
+    const page = 0
+    const size = 5
+
+    beforeAll(async () => {
+      receivedData = await api.get(`/api/stations?page=${page}&size=${size}`)
+    })
+
+    describe('if successful', () => {
+      test('responds with JSON', () => {
+        expect(receivedData.type).toBe('application/json')
+      })
+      test('responds with status code 200', () => {
+        expect(receivedData.statusCode).toBe(200)
+      })
+      describe('reponse object', () => {
+        test('has the expected number of keys', () => {
+          const keysInObject = Object.keys(receivedData.body)
+          expect(keysInObject).toHaveLength(EXPECTED_RETURN_OBJECT_KEYS.length)
+        })
+        test('has the expected properties and values (excluding station data)', () => {
+          const responseObject = receivedData.body
+
+          const expectedObject: { [index: string]: unknown } = {
+            totalNOfRows: 10,
+            page: 0,
+            pageSize: 5,
+            nextPage: 1,
+            totalPages: 2,
+          }
+
+          for (const key of Object.keys(responseObject)) {
+            if (key !== 'data') {
+              expect(responseObject[key]).toBe(expectedObject[key])
+            }
+          }
+        })
+        describe('data property', () => {
+          test('has the expected length', () => {
+            expect(receivedData.body.data).toHaveLength(size)
+          })
+          test('has expected stations', () => {
+            const expectedStations = stationsInCamelCaseWithSetDatabaseId.slice(
+              0,
+              size
+            )
+
+            for (const station of expectedStations) {
+              expect(receivedData.body.data).toContainEqual(station)
+            }
+          })
         })
       })
     })
