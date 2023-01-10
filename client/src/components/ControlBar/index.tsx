c2647931218fd7a2fa0e2977238d93afcad78e76
mikko <mikko.e.hyyrylainen@gmail.dom>
2023-01-10 15:02:30 +0200

    remove unnecessary import

--- client/src/components/ControlBar/index.tsx
+++ client/src/components/ControlBar/index.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
 import LanguageSelector from '../LanguageSelector'
 import NavigationButton from './NavigationButton'
 
