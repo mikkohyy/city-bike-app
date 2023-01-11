6b9657e76395787d5b1ba19fe51ca091f1c39a86
mikko <mikko.e.hyyrylainen@gmail.dom>
2023-01-11 14:33:12 +0200

    change test from javascript to typescript

--- client/src/__tests__/components/ControlBar/LanguageSelector.test.tsx
+++ client/src/__tests__/components/ControlBar/LanguageSelector.test.tsx
@@ -3,14 +3,20 @@ import LanguageSelector from '../../../components/ControlBar/LanguageSelector'
 import { ThemeProvider } from 'styled-components'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import { Language } from '../../../../types'
 
-const languages = ['Finnish', 'Swedish', 'English', 'American english']
-const defaultValue = 'English'
+const languages: Language[] = ['Finnish', 'Swedish', 'English']
+const defaultValue: Language = 'English'
+const mockSetLanguage = jest.fn()
 
 describe('<LanguageSelector>', () => {
   beforeEach(() => {
     render(
-      <LanguageSelector values={languages} defaultValue={defaultValue} />,
+      <LanguageSelector
+        values={languages}
+        defaultValue={defaultValue}
+        setLanguage={mockSetLanguage}
+      />,
       {
         wrapper: ({ children }) => (
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
@@ -28,18 +34,16 @@ describe('<LanguageSelector>', () => {
     const finnishOption = screen.getByRole('option', { name: 'Finnish' })
     const swedishOption = screen.getByRole('option', { name: 'Swedish' })
     const englishOption = screen.getByRole('option', { name: 'English' })
-    const americanEnglishOption = screen.getByRole('option', {
-      name: 'American english',
-    })
 
     expect(finnishOption).toBeDefined()
     expect(swedishOption).toBeDefined()
     expect(englishOption).toBeDefined()
-    expect(americanEnglishOption).toBeDefined()
   })
 
   test('has expected default value', () => {
-    const defaultOption = screen.getByRole('option', { name: defaultValue })
+    const defaultOption = screen.getByRole('option', {
+      name: defaultValue,
+    }) as HTMLOptionElement
     expect(defaultOption.selected).toBe(true)
   })
 })
