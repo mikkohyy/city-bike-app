893d25bf667e008e7bb18558c726a5f7d43b8ffe
mikko <mikko.e.hyyrylainen@gmail.dom>
2023-01-17 20:02:33 +0200

    refactor

--- server/controllers/stations.ts
+++ server/controllers/stations.ts
@@ -1,34 +1,12 @@
 import express from 'express'
 import { Station } from '../models'
-import { getIntegerParameter, getStringParameter } from '../utils/generic'
+import { getIntegerParameter } from '../utils/generic'
 const stationsRouter = express.Router()
 import { validateRequestParameters } from '../utils/middleware'
-import { Op } from 'sequelize'
-import { ParsedQs } from 'qs'
+import { getSearchParametersObject } from './stations_utils'
 
-// TÄSSÄ EI OLE JÄRKEÄ
-interface WhereObjectTypes {
-  nameInFinnish?: Record<symbol, string>
-}
-
-interface QueryObject {
-  page: number | undefined
-  pageSize: number | undefined
-  search: string | undefined
-  language: string | undefined
-  orderBy: string | undefined
-  orderDirection: string | undefined
-}
-
-interface FindAllObjectTypes {
-  offset?: number
-  limit?: number
-  order?: string[][]
-  where?: WhereObjectTypes
-}
-
-const DEFAULT_PAGE = 0
-const DEFAULT_PAGE_SIZE = 50
+export const DEFAULT_PAGE = 0
+export const DEFAULT_PAGE_SIZE = 50
 
 stationsRouter.get(
   '/',
@@ -62,138 +40,5 @@ stationsRouter.get(
     }
   }
 )
-
-const getSearchParametersObject = (query: ParsedQs): FindAllObjectTypes => {
-  const queryParameters = getQueryObject(query)
-
-  let parameters: FindAllObjectTypes = {
-    offset: getOffset(queryParameters.page, queryParameters.pageSize),
-    limit: getLimit(queryParameters.pageSize),
-  }
-
-  parameters = addOrderingParametersIfNeeded(parameters, queryParameters)
-
-  const searchTerm = getStringParameter(query.search)
-  const language = getStringParameter(query.language)
-
-  if (searchTerm && language) {
-    const nameKey = getLanguageKeyForStationName(language)
-    const whereObject: WhereObjectTypes = {
-      [nameKey]: { [Op.iLike]: `${searchTerm}%` },
-    }
-
-    parameters = { ...parameters, where: { ...whereObject } }
-  }
-
-  return parameters
-}
-
-const addOrderingParametersIfNeeded = (
-  parameters: FindAllObjectTypes,
-  queryParameters: QueryObject
-): FindAllObjectTypes => {
-  const { orderBy, orderDirection, language } = queryParameters
-
-  if (
-    orderBy &&
-    orderDirection &&
-    hasNecessaryOrderingParameters(orderBy, language)
-  ) {
-    const orderingArray = getOrderParameters(orderBy, orderDirection, language)
-    parameters = { ...parameters, order: orderingArray }
-  }
-
-  return parameters
-}
-
-const getOffset = (
-  page: number | undefined,
-  pageSize: number | undefined
-): number => {
-  const offset =
-    page !== undefined && pageSize !== undefined
-      ? page * pageSize
-      : DEFAULT_PAGE * DEFAULT_PAGE_SIZE
-
-  return offset
-}
-
-const getLimit = (pageSize: number | undefined): number => {
-  return pageSize !== undefined ? pageSize : DEFAULT_PAGE_SIZE
-}
-
-const getOrderParameters = (
-  orderBy: string,
-  orderDirection: string,
-  language: string | undefined
-): string[][] => {
-  const byColumn = getOrderingColumnName(orderBy, language)
-  const direction = orderDirection === 'ascending' ? 'ASC' : 'DESC'
-
-  const orderParameters = [[byColumn, direction]]
-
-  return orderParameters
-}
-
-const getQueryObject = (query: ParsedQs): QueryObject => {
-  const queryObject = {
-    page: getIntegerParameter(query.page),
-    pageSize: getIntegerParameter(query.pageSize),
-    search: getStringParameter(query.search),
-    language: getStringParameter(query.language),
-    orderBy: getStringParameter(query.orderBy),
-    orderDirection: getStringParameter(query.orderDirection),
-  }
-
-  return queryObject
-}
-
-const getOrderingColumnName = (
-  orderBy: string,
-  language: string | undefined
-): string => {
-  const columnChoices: { [key: string]: string } = {
-    stationId: 'stationId',
-  }
-
-  const column =
-    orderBy !== 'stationName'
-      ? columnChoices[orderBy]
-      : getLanguageKeyForStationName(language)
-
-  return column
-}
-
-const hasNecessaryOrderingParameters = (
-  orderBy: string | undefined,
-  language: string | undefined
-) => {
-  let shouldAdd = false
-
-  if (orderBy === 'stationId') {
-    shouldAdd = true
-  } else if (orderBy === 'stationName' && language) {
-    shouldAdd = true
-  }
-
-  return shouldAdd
-}
-
-const getLanguageKeyForStationName = (language: string | undefined): string => {
-  if (language === undefined) {
-    throw new Error(
-      'When ordering by column name, language parameter is needed'
-    )
-  }
-  const languageInLowCase = language.toLowerCase()
-
-  const keys: Record<string, string> = {
-    finnish: 'nameInFinnish',
-    swedish: 'nameInSwedish',
-    english: 'nameInEnglish',
-  }
-
-  return keys[languageInLowCase]
-}
 
 export default stationsRouter
---
+++ server/controllers/stations_utils.ts
@@ -0,0 +1,148 @@
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from './stations'
+import { getIntegerParameter, getStringParameter } from '../utils/generic'
+import { ParsedQs } from 'qs'
+import { Op } from 'sequelize'
+
+interface QueryObject {
+  page: number | undefined
+  pageSize: number | undefined
+  searchTerm: string | undefined
+  language: string | undefined
+  orderBy: string | undefined
+  orderDirection: string | undefined
+}
+
+interface FindAllObjectTypes {
+  offset?: number
+  limit?: number
+  order?: string[][]
+  where?: object
+}
+
+const getSearchParametersObject = (query: ParsedQs): FindAllObjectTypes => {
+  const queryParameters = getQueryObject(query)
+
+  let parameters: FindAllObjectTypes = {
+    offset: getOffset(queryParameters),
+    limit: getLimit(queryParameters),
+  }
+
+  parameters = addOrderingParametersIfNeeded(parameters, queryParameters)
+  parameters = addSearchParametersIfNeeded(parameters, queryParameters)
+
+  return parameters
+}
+
+const addOrderingParametersIfNeeded = (
+  parameters: FindAllObjectTypes,
+  queryParameters: QueryObject
+): FindAllObjectTypes => {
+  const { orderBy, orderDirection, language } = queryParameters
+
+  if (orderBy && orderDirection) {
+    const orderingArray = getOrderParameters(orderBy, orderDirection, language)
+    parameters = { ...parameters, order: orderingArray }
+  }
+
+  return parameters
+}
+
+const addSearchParametersIfNeeded = (
+  parameters: FindAllObjectTypes,
+  queryParameters: QueryObject
+): FindAllObjectTypes => {
+  const { searchTerm, language } = queryParameters
+
+  if (searchTerm && language) {
+    const nameKey = getLanguageKeyForStationName(language)
+    const whereObject = {
+      [nameKey]: { [Op.iLike]: `${searchTerm}%` },
+    }
+
+    parameters = { ...parameters, where: { ...whereObject } }
+  }
+
+  return parameters
+}
+
+const getOffset = (queryParameters: QueryObject): number => {
+  const { page, pageSize } = queryParameters
+  const offset =
+    page !== undefined && pageSize !== undefined
+      ? page * pageSize
+      : DEFAULT_PAGE * DEFAULT_PAGE_SIZE
+
+  return offset
+}
+
+const getLimit = (queryParameters: QueryObject): number => {
+  const { pageSize } = queryParameters
+  return pageSize !== undefined ? pageSize : DEFAULT_PAGE_SIZE
+}
+
+const getOrderParameters = (
+  orderBy: string,
+  orderDirection: string,
+  language: string | undefined
+): string[][] => {
+  if (orderBy === 'stationName' && language === undefined) {
+    throw new Error(
+      'When ordering by station name, language must also be provided."'
+    )
+  }
+
+  const byColumn = getOrderingColumnName(orderBy, language)
+  const direction = orderDirection === 'ascending' ? 'ASC' : 'DESC'
+
+  const orderParameters = [[byColumn, direction]]
+
+  return orderParameters
+}
+
+const getOrderingColumnName = (
+  orderBy: string,
+  language: string | undefined
+): string => {
+  const columnChoices: { [key: string]: string } = {
+    stationId: 'stationId',
+  }
+
+  const column =
+    orderBy === 'stationName'
+      ? getLanguageKeyForStationName(language)
+      : columnChoices[orderBy]
+
+  return column
+}
+
+const getLanguageKeyForStationName = (language: string | undefined): string => {
+  if (language === undefined) {
+    throw new Error(
+      'When ordering by column name, language parameter is needed'
+    )
+  }
+  const languageInLowCase = language.toLowerCase()
+
+  const keys: Record<string, string> = {
+    finnish: 'nameInFinnish',
+    swedish: 'nameInSwedish',
+    english: 'nameInEnglish',
+  }
+
+  return keys[languageInLowCase]
+}
+
+const getQueryObject = (query: ParsedQs): QueryObject => {
+  const queryObject = {
+    page: getIntegerParameter(query.page),
+    pageSize: getIntegerParameter(query.pageSize),
+    searchTerm: getStringParameter(query.search),
+    language: getStringParameter(query.language),
+    orderBy: getStringParameter(query.orderBy),
+    orderDirection: getStringParameter(query.orderDirection),
+  }
+
+  return queryObject
+}
+
+export { getSearchParametersObject }
